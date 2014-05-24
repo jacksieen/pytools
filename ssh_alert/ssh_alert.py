e3f40f86410f5fb129728f8134a62b89113f6251
jacksieen <jacksonc1117@gmail.com>
2014-05-24 11:36:45 +0800

    create settings module

--- ssh_alert/ssh_alert.py
+++ ssh_alert/ssh_alert.py
@@ -5,28 +5,7 @@ from IPy import IP
 import string, time, re, os, sys
 from datetime import *
 import MySQLdb
-
-LOG_FILE =          "/var/log/message"
-DB_HOST =           "localhost"
-DB_NAME =           "db"
-DB_TABLE =          "table"
-DB_USER =           "user"
-DB_PASSWORD =       "upass"
-
-def getpwd():
-    pwd = sys.path[0]
-    if os.path.isfile(pwd):
-        pwd = os.path.dirname(pwd)
-    return pwd
-
-ADMIN_EMAIL =    'admin@localhost'
-SMTP_SERVER =    'localhost'
-SMTP_LOGIN =     'admin@localhost'
-SMTP_PASSWD =    'pass'
-SMTP_SUBJECT =   'SERVER LOGIN NOTICE'
-WORKING_DIR =    getpwd() + '/'
-WHITE_LIST =     WORKING_DIR + 'white_list'
-ALERT_TEMPLATE = 'User %s has logined %s from %s at %s'
+from settings import *
 
 mail_content = []
 wlist = []
